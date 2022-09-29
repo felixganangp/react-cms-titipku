0ba48be23ae81ec325aaa1ac728428282a254825
Asra Avisena <asra.avisena@titipku.com>
2022-09-29 14:38:23 +0700

    setup config for eslint

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const NotFound: React.FC = () => {
   return (
-    <div>NotFound<span>gimana</span></div>
+    <div>
+      NotFound<span>gimana</span>
+    </div>
   );
-}
+};
 
 export default NotFound;
