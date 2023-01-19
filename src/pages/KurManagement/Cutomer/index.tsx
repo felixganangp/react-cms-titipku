4f302c965a519ce881fa66f57d176bb9012a78aa
Asra Avisena <asra.avisena@titipku.com>
2023-01-19 11:23:02 +0700

    CMS-781: add component table

--- src/pages/KurManagement/Cutomer/index.tsx
+++ src/pages/KurManagement/Cutomer/index.tsx
@@ -1,7 +1,131 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import {
+  Card,
+  Box,
+  Grid,
+  Button,
+  TextField,
+  Typography,
+  InputAdornment,
+} from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import SearchIcon from '@mui/icons-material/Search';
+
+import { useAppDispatch, useAppSelector } from 'store/hooks';
+import { skuManagementAction } from 'store/slice/SkuManagement';
+import useModal from 'hooks/useModal';
+import Table from 'components/Table';
+import Modal from 'components/Modal';
 
 // type Props = {};
 
-export default function index() {
-  return <div>index</div>;
+export default function KurCustomer() {
+  const skus = useAppSelector((state: any) => state.skuManagement.data);
+  // React.useEffect(() => {
+  //   dispatch(skuManagementAction.fetchData());
+  // }, []);
+  const formModal = useModal();
+
+  const headCell = [
+    {
+      id: 'image',
+      label: 'Image',
+      align: 'center',
+      format: (val: any) => (
+        <div
+          aria-hidden="true"
+          style={{
+            cursor: 'pointer',
+            height: '100%',
+            position: 'relative',
+          }}
+        >
+          {/* <Image src={val.image} alt={val.sku_name} /> */}
+        </div>
+      ),
+    },
+    {
+      id: 'sku_name',
+      label: 'SKU Name',
+      align: 'center',
+    },
+    {
+      id: 'origin',
+      label: 'Origin',
+      align: 'center',
+    },
+    {
+      id: 'weight',
+      label: 'Weight Per Pcs (Gram)',
+      align: 'center',
+    },
+    {
+      id: 'available_stock',
+      label: 'Available Stock',
+      align: 'center',
+    },
+    {
+      id: 'last_supply_date',
+      label: 'Last Supply Date',
+      align: 'center',
+    },
+    {
+      id: 'action',
+      label: 'Action',
+      align: 'center',
+    },
+  ];
+  return (
+    <Box p="20px" bgcolor="#F5F7FA">
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Card>
+            <Typography variant="titlePage">KUR Customer</Typography>
+          </Card>
+        </Grid>
+        <Grid item xs={12}>
+          <Card>
+            <Box display="flex" gap="20px" flexWrap="wrap">
+              <Button startIcon={<AddIcon />} onClick={formModal.openModal}>
+                Add Customer
+              </Button>
+              <TextField
+                placeholder="Search for customer name"
+                size="small"
+                sx={{ bgcolor: '#ebeff3', maxWidth: '560px' }}
+                fullWidth
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <SearchIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Box>
+          </Card>
+        </Grid>
+        <Grid item xs={12}>
+          <Box
+            bgcolor="#fff"
+            p="7px"
+            borderRadius="5px"
+            boxShadow="0 3px 10px 0 rgba(0, 0, 0, 0.1)"
+          >
+            <Table
+              data={skus}
+              selected={[]}
+              headCells={headCell}
+              page={1}
+              totalData={10}
+              onChangePage={(e) => console.log(e)}
+              // loading
+              enableCheckBox
+            />
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
