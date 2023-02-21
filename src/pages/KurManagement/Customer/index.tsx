c4f1a850569a205c8f9b000807e175f4f723a99e
Asra Avisena <asra.avisena@titipku.com>
2023-02-21 11:21:21 +0700

    CMS-772: add testing for hold and fix error test

--- src/pages/KurManagement/Customer/index.tsx
+++ src/pages/KurManagement/Customer/index.tsx
@@ -335,6 +335,7 @@ export default function KurCustomer() {
                 onClick: () => {
                   handleHoldCustomer(val);
                 },
+                dataId: 'button-hold-customer',
               },
             ]}
           >
