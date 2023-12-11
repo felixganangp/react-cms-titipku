f0a9eeb444cb0ea89108b67934e7f73fa5847999
ridhotitipku <107243155+ridhotitipku@users.noreply.github.com>
2023-12-11 09:56:30 +0700

    finance update

--- src/app/Router/FinaceRouter.tsx
+++ src/app/Router/FinaceRouter.tsx
@@ -4,6 +4,15 @@ import type { RouteProps } from './ListRoute';
 const InvoicePage = lazy(() => import('pages/Finance/Invoice'));
 const InvoiceDetailsPage = lazy(() => import('pages/Finance/Invoice/details'));
 const PaymentPage = lazy(() => import('pages/Finance/Payment'));
+const KurCustomerManagement = lazy(
+  () => import('pages/KurManagement/Customer/Customer'),
+);
+const KurCustomerDetailsManagement = lazy(
+  () => import('pages/KurManagement/Customer/Customer/Details'),
+);
+const KurCustomerVerification = lazy(
+  () => import('pages/KurManagement/Customer/Verification'),
+);
 
 export const FinaceRouters: RouteProps[] = [
   {
@@ -24,6 +33,27 @@ export const FinaceRouters: RouteProps[] = [
     name: 'Invoice',
     comp: PaymentPage,
     path: '/finance/payment',
+    index: true,
+    auth: 'Private',
+  },
+  {
+    name: 'Customer',
+    comp: KurCustomerManagement,
+    path: '/finance/customer',
+    index: true,
+    auth: 'Private',
+  },
+  {
+    name: 'Customer',
+    comp: KurCustomerDetailsManagement,
+    path: '/finance/customer/:id',
+    index: true,
+    auth: 'Private',
+  },
+  {
+    name: 'Verification',
+    comp: KurCustomerVerification,
+    path: '/finance/verification',
     index: true,
     auth: 'Private',
   },
