610b489354da4d47176834fc44734130212a93e5
Felix Ganang <>
2024-01-05 14:27:50 +0700

    Fix customer model

--- src/models/kur/Customer.ts
+++ src/models/kur/Customer.ts
@@ -38,6 +38,93 @@ export interface CreateCustomer {
   kurUserStatus: string | undefined;
 }
 
+export type CustomerDetail = {
+  id: number;
+  user_number: string;
+  debtor_name: string;
+  merchant_name: string;
+  request_number: string;
+  phone_number: string;
+  family_phone_number: string;
+  business_lifetime: number;
+  marriage_status: string;
+  marriage_partner_name: string;
+  limit_request_plafon: number;
+  limit_request_cash: number;
+  limit_plafon: number;
+  limit_cash: number;
+  user_type_id: number;
+  user_status_id: number;
+  bi_checking_status: string;
+  bi_checking_notes: string;
+  komite_notes: string;
+  is_merchant_titipku: boolean;
+  area_id: number;
+  area_name: string;
+  created_by: number;
+  last_co_id: null | number;
+  last_komite_id: null | number;
+  last_admin_id: null | number;
+  category_jelajah_id: number;
+  category_jelajah_name: string;
+  batch_id: null | number;
+  balance: number;
+  created_at: number;
+  updated_at: null;
+  deleted_at: null;
+  user_status: CustomerStatus;
+  user_type: CustomerType;
+  user_idir: CustomerIdir;
+  user_documents: CustomerDocument[];
+  user_status_history: CustomerStatusHistory[];
+  user_geotag_images: GeoLocation[];
+};
+
+export type CustomerDocument = {
+  id: number;
+  user_id: number;
+  document_type_id: number;
+  document_number: string;
+  image_filepath: string;
+  created_at: number;
+  updated_at: null;
+  deleted_at: null;
+};
+
+export type GeoLocation = {
+  id: number;
+  user_id: number;
+  image_filepath: string;
+  created_at: number;
+};
+
+export type CustomerIdir = {
+  UserId: number;
+  IdirNumber: string;
+  GMV: number;
+  OfficeRent: number;
+  Electricity: number;
+  EducationExpense: number;
+  HouseholdExpense: number;
+  EmployeeExpense: number;
+  AnotherLoan: number;
+  CashOutPurpose: string;
+  IdirScore: null;
+  IdirNotes: null;
+  CreatedAt: number;
+  UpdatedAt: null;
+  DeletedAt: null;
+};
+
+export type CustomerStatusHistory = {
+  id: number;
+  user_id: number;
+  user_status_id: number;
+  admin_id: number;
+  admin_type_id: number;
+  created_at: number;
+};
+
 export interface Customer {
   id?: number;
   user_number: string;
