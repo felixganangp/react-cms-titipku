bbf8c603d9d1b7bac83fe55664bbc343afd5f196
ridhotitipku <107243155+ridhotitipku@users.noreply.github.com>
2024-02-23 14:26:41 +0700

    update form bank list

--- src/pages/MerchantsDepo/hooks/useConfigMerchant.tsx
+++ src/pages/MerchantsDepo/hooks/useConfigMerchant.tsx
@@ -5,6 +5,7 @@ import {
   getAllFilterMerchantDepo,
   getAllTypeMerchantDepo,
   getAllMutationTypeMerchantDepo,
+  getAllBankListMerchant,
 } from 'service/MerchantDepo/Config';
 import { useQuery } from '@tanstack/react-query';
 import UseParams from 'hooks/useParams';
@@ -23,6 +24,16 @@ export function UseAreaListService(setParams?: ListParams) {
   const query = useQuery({
     queryKey: ['merchant-depo/area', params.params],
     queryFn: () => getAllAreaMerchantDepo(params.params),
+  });
+  return { ...query, ...params, listData: query?.data?.data || [] };
+}
+
+export function UseBankListService(setParams?: ListParams) {
+  const params = UseParams(setParams);
+
+  const query = useQuery({
+    queryKey: ['merchant-depo/bank-list', params.params],
+    queryFn: () => getAllBankListMerchant(params.params),
   });
   return { ...query, ...params, listData: query?.data?.data || [] };
 }
